fc7f869f96e1b777d542ca2e2a79bfc2d187f64b
TheoLechemia <lechemia.theo@gmail.com>
2024-08-14 23:57:59 +0200

    admin site

--- backend/openstep/step/admin.py
+++ backend/openstep/step/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.gis.admin import GISModelAdmin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+
+from step.models import Step, Travel, Media
+
+class MediaInline(admin.TabularInline):
+    model = Media
+    extra = 0
+    fields = ("legend", "media_file", "media_preview")
+    readonly_fields = ("media_preview",)
+
+    @admin.display(description="Preview")
+    def media_preview(self, obj):
+        return mark_safe(
+            f'<img src="{obj.media_file.url}" style="object-fit:contain" width="150" />'
+        )
+
+
+class StepAdmin(GISModelAdmin):
+    inlines = [MediaInline]
+    list_display = ("name", "description", "media_preview",)
+
+    @admin.display(description="Media")
+    def media_preview(self, obj):
+        if obj.first_media:
+            return mark_safe(
+                f'<img src="{obj.first_media.media_file.url}" style="object-fit:contain" width="150" />'
+            )
+
+
+
+class TravelAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Step, StepAdmin)
+admin.site.register(Travel, TravelAdmin)
+
--- backend/openstep/step/models.py
+++ backend/openstep/step/models.py
@@ -1,23 +1,38 @@
 from django.contrib.gis.db import models
 from django.utils.translation import gettext_lazy as _
 
-
 # Create your models here.
 
 class Travel(models.Model):
     name = models.CharField(max_length=200)
     description =  models.CharField()
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    main_photo = models.CharField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    main_photo = models.CharField(blank=True, null=True)
 
+    def __str__(self) -> str:
+        return self.name
 
-class Media(models.Model):
-    name = models.CharField()
-    path = models.CharField()
 
 class Step(models.Model):
     name = models.CharField()
     location = models.PointField(srid=4326, verbose_name=_("Location"))
-    descriptions = models.CharField()
-    medias = models.ForeignKey(Media, on_delete=models.CASCADE)
+    description = models.CharField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    @property
+    def first_media(self):
+        return self.medias.first()
+    
+
+class Media(models.Model):
+    legend = models.CharField()
+    media_file = models.FileField(upload_to="static", verbose_name=_("File"))
+
+    step = models.ForeignKey(
+        Step, 
+        on_delete=models.CASCADE, 
+        related_name="medias",
+    )
